cc36f35a09ab23fea6a1cf1b4f0d608a67d402a0
TheoAt <theo.atlani@gmail.com>
2024-03-28 21:31:10 +0100

    initialization react projects with components and few CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+//Import des différents composants
+import Main from './components/Main.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <div className="App">
+        <Main />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main() {
+    return(
+        <div className="main_content">
+            <h1 className="main_title">Création de factures en PDF</h1>
+            <h2 className="subtitles">par TheoDev</h2>
+        </div>
+    )
+}
